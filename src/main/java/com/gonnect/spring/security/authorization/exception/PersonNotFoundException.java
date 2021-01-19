2f5f711df1750aaa5db56d748d5447867e8a205d
unknown <bshashaank@gmail.com>
2021-01-19 17:30:24 -0500

    Custom exception

--- src/main/java/com/gonnect/spring/security/authorization/exception/PersonNotFoundException.java
+++ src/main/java/com/gonnect/spring/security/authorization/exception/PersonNotFoundException.java
@@ -1,7 +1,9 @@
 package com.gonnect.spring.security.authorization.exception;
 
 public class PersonNotFoundException extends RuntimeException {
-    public PersonNotFoundException(String message) {
+    
+	//Custom exception
+   public PersonNotFoundException(String message) {
         super(message);
     }
 }
